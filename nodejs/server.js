94b8c02129b14f83ed5d970a60b4c8c03404b4c3
SangJune0627 <yain20301@gmail.com>
2021-01-08 13:24:18 +0000

    Http connection init

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,13 +1,75 @@
-// 1. 서버 사용을 위해서 http 모듈을 http 변수에 담는다. (모듈과 변수의 이름은 달라도 된다.) 
 var http = require('http'); 
+var mongoose = require('mongoose');
+var querystring = require('querystring');
 
-// 2. http 모듈로 서버를 생성한다.
-//    아래와 같이 작성하면 서버를 생성한 후, 사용자로 부터 http 요청이 들어오면 function 블럭내부의 코드를 실행해서 응답한다.
+mongoose.connect('mongodb://localhost:27017/testDB',{ useNewUrlParser: true , useUnifiedTopology: true });
+var db = mongoose.connection;
+
+db.on('error', function(){
+    console.log('Connection Failed!');
+});
+
+db.once('open', function() {
+    console.log('Connected!');
+});
+
+var teststring = mongoose.Schema({
+    string : 'string'
+});
+
+var Teststring = mongoose.model('Schema', teststring);
+
 var server = http.createServer(function(request,response){ 
 
-    response.writeHead(200,{'Content-Type':'text/html'});
-    response.end('Hello node.js!!!');
+    if(request.method=='POST'){
 
+        console.log("POST Connected");
+
+        var postdata = '';
+
+        request.on('data', function (data) {
+            postdata = postdata + data;
+            console.log("Data Connected");
+        });
+
+        request.on('end', function () {
+
+            var newTestString = new Teststring({string:postdata});
+            var getStringInDb = '';
+            var id='';
+            newTestString.save(function(error, data){
+                if(error){
+                    console.log(error);
+                }else{
+                    console.log('Saved!');
+                    id=data._id;
+                }
+            });
+
+            Teststring.findOne({_id:id},function(error, string){
+                console.log('--- Read all ---');
+                if(error){
+                    console.log(error);
+                }else{
+                    console.log(string);
+                    getStringInDb = string;
+                }
+            })
+
+            response.writeHead(201, {'Content-Type':'text/html'});
+            response.end('Data is: ' + getStringInDb+ "\nIt works!");
+        });
+    }
+    else if(request.method=='GET'){
+        console.log("GET Connected");
+        response.writeHead(200, {'Content-Type':'text/html'});
+        response.end("Hello world!!!!~~~");
+    }
+    else{
+        console.log("????");
+        response.writeHead(404, {'Content-Type':'text/html'});
+        response.end("That's nono!");
+    }
 });
 
 // 3. listen 함수로 8080 포트를 가진 서버를 실행한다. 서버가 실행된 것을 콘솔창에서 확인하기 위해 'Server is running...' 로그를 출력한다
